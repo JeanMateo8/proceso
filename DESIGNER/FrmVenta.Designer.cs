a9b8e7974d83363b7596e48b21f506888f02a576
Jean Mateo <1359806@senati.com>
2023-03-07 09:19:43 -0500

    actualizando

---
+++ DESIGNER/FrmVenta.Designer.cs
@@ -0,0 +1,390 @@
+﻿namespace DESIGNER
+{
+    partial class FrmVenta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnagregar = new System.Windows.Forms.Button();
+            this.btnreiniciar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.txtcodigo = new System.Windows.Forms.TextBox();
+            this.txtdatoscliente = new System.Windows.Forms.TextBox();
+            this.txtdni = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtstock = new System.Windows.Forms.TextBox();
+            this.txtprecio = new System.Windows.Forms.TextBox();
+            this.txtcantidad = new System.Windows.Forms.TextBox();
+            this.txtfecha = new System.Windows.Forms.TextBox();
+            this.txtsubtotal = new System.Windows.Forms.TextBox();
+            this.txtigv = new System.Windows.Forms.TextBox();
+            this.txtneto = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txtidempleado = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DNI:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(215, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Datos del cliente";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(637, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Fecha Actual:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(79, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Código:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(215, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Descripción";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(215, 186);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Stock:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(332, 187);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Precio:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(462, 186);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Cantidad:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(263, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(282, 29);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "BOLETA DE VENTA";
+            // 
+            // btnagregar
+            // 
+            this.btnagregar.Location = new System.Drawing.Point(640, 131);
+            this.btnagregar.Name = "btnagregar";
+            this.btnagregar.Size = new System.Drawing.Size(100, 39);
+            this.btnagregar.TabIndex = 9;
+            this.btnagregar.Text = "Agregar";
+            this.btnagregar.UseVisualStyleBackColor = true;
+            // 
+            // btnreiniciar
+            // 
+            this.btnreiniciar.Location = new System.Drawing.Point(640, 186);
+            this.btnreiniciar.Name = "btnreiniciar";
+            this.btnreiniciar.Size = new System.Drawing.Size(100, 37);
+            this.btnreiniciar.TabIndex = 10;
+            this.btnreiniciar.Text = "Reiniciar";
+            this.btnreiniciar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 247);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(671, 197);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(640, 40);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(55, 17);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Boleta";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(717, 40);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(61, 17);
+            this.radioButton2.TabIndex = 13;
+            this.radioButton2.Text = "Factura";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // txtcodigo
+            // 
+            this.txtcodigo.Location = new System.Drawing.Point(82, 157);
+            this.txtcodigo.Name = "txtcodigo";
+            this.txtcodigo.Size = new System.Drawing.Size(70, 20);
+            this.txtcodigo.TabIndex = 14;
+            // 
+            // txtdatoscliente
+            // 
+            this.txtdatoscliente.Location = new System.Drawing.Point(218, 93);
+            this.txtdatoscliente.Name = "txtdatoscliente";
+            this.txtdatoscliente.Size = new System.Drawing.Size(327, 20);
+            this.txtdatoscliente.TabIndex = 14;
+            // 
+            // txtdni
+            // 
+            this.txtdni.Location = new System.Drawing.Point(82, 93);
+            this.txtdni.Name = "txtdni";
+            this.txtdni.Size = new System.Drawing.Size(70, 20);
+            this.txtdni.TabIndex = 14;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(218, 157);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(327, 20);
+            this.textBox4.TabIndex = 14;
+            // 
+            // txtstock
+            // 
+            this.txtstock.Location = new System.Drawing.Point(218, 203);
+            this.txtstock.Name = "txtstock";
+            this.txtstock.Size = new System.Drawing.Size(51, 20);
+            this.txtstock.TabIndex = 15;
+            // 
+            // txtprecio
+            // 
+            this.txtprecio.Location = new System.Drawing.Point(335, 203);
+            this.txtprecio.Name = "txtprecio";
+            this.txtprecio.Size = new System.Drawing.Size(80, 20);
+            this.txtprecio.TabIndex = 16;
+            // 
+            // txtcantidad
+            // 
+            this.txtcantidad.Location = new System.Drawing.Point(465, 203);
+            this.txtcantidad.Name = "txtcantidad";
+            this.txtcantidad.Size = new System.Drawing.Size(68, 20);
+            this.txtcantidad.TabIndex = 17;
+            // 
+            // txtfecha
+            // 
+            this.txtfecha.Location = new System.Drawing.Point(640, 93);
+            this.txtfecha.Name = "txtfecha";
+            this.txtfecha.Size = new System.Drawing.Size(100, 20);
+            this.txtfecha.TabIndex = 18;
+            // 
+            // txtsubtotal
+            // 
+            this.txtsubtotal.Location = new System.Drawing.Point(749, 338);
+            this.txtsubtotal.Name = "txtsubtotal";
+            this.txtsubtotal.Size = new System.Drawing.Size(72, 20);
+            this.txtsubtotal.TabIndex = 19;
+            // 
+            // txtigv
+            // 
+            this.txtigv.Location = new System.Drawing.Point(749, 373);
+            this.txtigv.Name = "txtigv";
+            this.txtigv.Size = new System.Drawing.Size(72, 20);
+            this.txtigv.TabIndex = 20;
+            // 
+            // txtneto
+            // 
+            this.txtneto.Location = new System.Drawing.Point(749, 408);
+            this.txtneto.Name = "txtneto";
+            this.txtneto.Size = new System.Drawing.Size(72, 20);
+            this.txtneto.TabIndex = 21;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(700, 345);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(49, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Subtotal:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(715, 383);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(28, 13);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "IGV:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(712, 414);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(33, 13);
+            this.label12.TabIndex = 24;
+            this.label12.Text = "Neto:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(21, 457);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(40, 13);
+            this.label13.TabIndex = 25;
+            this.label13.Text = "Cajero:";
+            // 
+            // txtidempleado
+            // 
+            this.txtidempleado.Location = new System.Drawing.Point(67, 450);
+            this.txtidempleado.Name = "txtidempleado";
+            this.txtidempleado.Size = new System.Drawing.Size(398, 20);
+            this.txtidempleado.TabIndex = 26;
+            // 
+            // FrmVenta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 510);
+            this.Controls.Add(this.txtidempleado);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtneto);
+            this.Controls.Add(this.txtigv);
+            this.Controls.Add(this.txtsubtotal);
+            this.Controls.Add(this.txtfecha);
+            this.Controls.Add(this.txtcantidad);
+            this.Controls.Add(this.txtprecio);
+            this.Controls.Add(this.txtstock);
+            this.Controls.Add(this.txtdatoscliente);
+            this.Controls.Add(this.txtdni);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.txtcodigo);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnreiniciar);
+            this.Controls.Add(this.btnagregar);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmVenta";
+            this.Text = "FrmVenta";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnagregar;
+        private System.Windows.Forms.Button btnreiniciar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox txtcodigo;
+        private System.Windows.Forms.TextBox txtdatoscliente;
+        private System.Windows.Forms.TextBox txtdni;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtstock;
+        private System.Windows.Forms.TextBox txtprecio;
+        private System.Windows.Forms.TextBox txtcantidad;
+        private System.Windows.Forms.TextBox txtfecha;
+        private System.Windows.Forms.TextBox txtsubtotal;
+        private System.Windows.Forms.TextBox txtigv;
+        private System.Windows.Forms.TextBox txtneto;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox txtidempleado;
+    }
+}
